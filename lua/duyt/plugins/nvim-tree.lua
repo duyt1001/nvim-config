9c3551f013750ef627be0e1d366c194a9843e499
Yuantai Du <duyt1001@gmail.com>
2026-03-20 01:33:46 -0400

    nvim-tree auto exit if it's last window

--- lua/duyt/plugins/nvim-tree.lua
+++ lua/duyt/plugins/nvim-tree.lua
@@ -43,5 +43,28 @@ return {
     end
 
     vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+    -- Auto-close nvim-tree if it's the last window
+    vim.api.nvim_create_autocmd("QuitPre", {
+      callback = function()
+        local tree_wins = {}
+        local floating_wins = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+          if bufname:match("NvimTree_") ~= nil then
+            table.insert(tree_wins, w)
+          end
+          if vim.api.nvim_win_get_config(w).relative ~= "" then
+            table.insert(floating_wins, w)
+          end
+        end
+        if #wins - #floating_wins - #tree_wins == 1 then
+          for _, w in ipairs(tree_wins) do
+            vim.api.nvim_win_close(w, true)
+          end
+        end
+      end,
+    })
   end,
 }
